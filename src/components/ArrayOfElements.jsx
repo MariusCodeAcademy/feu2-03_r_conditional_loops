d650abd5de19d68241816b0a21f61aabfb22a266
Marius <marius@example.com>
2022-11-23 10:40:44 +0200

    color name

--- src/components/ArrayOfElements.jsx
+++ src/components/ArrayOfElements.jsx
@@ -1,15 +1,18 @@
 const colors = ['red', 'green', 'blue', 'yellow'];
 
 function ArrayOfElements() {
+  const h2Styles = {
+    fontSize: '3rem',
+    color: 'tomato',
+  };
   return (
     <div>
-      <h2>ArrayOfElements</h2>
+      <h2 style={h2Styles}>ArrayOfElements</h2>
       <ul>
-        <li>Once</li>
-        <li>Once</li>
-        <li>Once</li>
-        <li>Once</li>
-        <li>Once</li>
+        {colors.map((colorName) => {
+          // validation computation
+          return <li style={{ color: colorName }}>{colorName}</li>;
+        })}
       </ul>
     </div>
   );
